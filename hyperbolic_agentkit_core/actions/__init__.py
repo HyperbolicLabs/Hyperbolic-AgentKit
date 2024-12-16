0a65062a938179ae86f321c5af68c49a673b22d3
sagarspatil <sagarpatil86@gmail.com>
2024-12-16 12:12:44 +0530

    Add Holesky Ethereum Validator Node Deployment and Management via Hyperbolic Platform

--- hyperbolic_agentkit_core/actions/__init__.py
+++ hyperbolic_agentkit_core/actions/__init__.py
@@ -1,18 +1,16 @@
-from hyperbolic_agentkit_core.actions.hyperbolic_action import HyperbolicAction
-from hyperbolic_agentkit_core.actions.rent_compute import RentComputeAction
+from hyperbolic_agentkit_core.actions.deploy_validator_node import (
+    DeployValidatorNodeAction,
+)
 from hyperbolic_agentkit_core.actions.get_available_gpus import GetAvailableGpusAction
 from hyperbolic_agentkit_core.actions.get_gpu_status import GetGpuStatusAction
-from hyperbolic_agentkit_core.actions.ssh_access import SSHAccessAction
+from hyperbolic_agentkit_core.actions.hyperbolic_action import HyperbolicAction
 from hyperbolic_agentkit_core.actions.remote_shell import RemoteShellAction
 from hyperbolic_agentkit_core.actions.get_spend_history import GetSpendHistoryAction
 from hyperbolic_agentkit_core.actions.terminate_compute import TerminateComputeAction
 from hyperbolic_agentkit_core.actions.get_current_balance import GetCurrentBalanceAction
 
 
-# WARNING: All new HyperbolicAction subclasses must be imported above, otherwise they will not be discovered
-# by get_all_hyperbolic_actions(). The import ensures the class is registered as a subclass of HyperbolicAction.
 def get_all_hyperbolic_actions() -> list[type[HyperbolicAction]]:
-    """Retrieve all subclasses of HyperbolicAction defined in the package."""
     actions = []
     for action in HyperbolicAction.__subclasses__():
         actions.append(action())
@@ -22,6 +20,15 @@ def get_all_hyperbolic_actions() -> list[type[HyperbolicAction]]:
 HYPERBOLIC_ACTIONS = get_all_hyperbolic_actions()
 
 __all__ = [
-    "HYPERBOLIC_ACTIONS", "HyperbolicAction", "RentComputeAction", "GetAvailableGpusAction",
-    "GetGpuStatusAction", "SSHAccessAction", "RemoteShellAction", "TerminateComputeAction", "GetSpendHistoryAction", "GetCurrentBalanceAction"
+    "HYPERBOLIC_ACTIONS",
+    "HyperbolicAction",
+    "RentComputeAction",
+    "GetAvailableGpusAction",
+    "GetGpuStatusAction",
+    "SSHAccessAction",
+    "RemoteShellAction", 
+    "TerminateComputeAction", 
+    "GetSpendHistoryAction", 
+    "GetCurrentBalanceAction",
+    "DeployValidatorNodeAction",
 ]
