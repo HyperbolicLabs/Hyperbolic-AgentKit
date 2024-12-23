43551fe6043b9f73f9fc753f665af3a35c6a1ee9
Amr Elhady <theamrelhady@gmail.com>
2024-12-23 12:14:50 -0500

    Update __init__.py

--- hyperbolic_agentkit_core/actions/__init__.py
+++ hyperbolic_agentkit_core/actions/__init__.py
@@ -4,8 +4,7 @@ from hyperbolic_agentkit_core.actions.get_available_gpus import GetAvailableGpus
 from hyperbolic_agentkit_core.actions.get_gpu_status import GetGpuStatusAction
 from hyperbolic_agentkit_core.actions.ssh_access import SSHAccessAction
 from hyperbolic_agentkit_core.actions.remote_shell import RemoteShellAction
-from hyperbolic_agentkit_core.actions.terminate_compute import TerminateComputeAction
-from hyperbolic_agentkit_core.actions.get_spend_history import GetSpendHistoryAction
+
 
 # WARNING: All new HyperbolicAction subclasses must be imported above, otherwise they will not be discovered
 # by get_all_hyperbolic_actions(). The import ensures the class is registered as a subclass of HyperbolicAction.
